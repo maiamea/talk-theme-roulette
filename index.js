537dac5af8ef545df157beed86be67a83adbd7be
maiamea <maiameawork@gmail.com>
2022-07-26 00:22:41 +0900

    feat: スタートボタンをクリックしたらあらかじめ設定したトークテーマが表示される

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 
 import talkThemesData from "./talk-themes-data.json" assert {type: "json"}
 console.log({talkThemesData});
+
+const startButton = document.getElementById('start-button');
+
+// スタートボタンをクリックすると、トークテーマが表示される
+startButton.onclick = () => {
+  console.log('ボタンが押されました。');
+  
+  // 決定したトークテーマを表示するエリアの作成
+  const resultArea = document.getElementById('result-area');
+  const header = document.createElement('h2');
+  header.innerHTML = 'トークテーマの発表';
+  const selectedTalkTheme = document.createElement('p');
+  selectedTalkTheme.innerHTML = 'トークテーマは「昨日の晩ごはん」です。';
+  resultArea.appendChild(header);
+  resultArea.appendChild(selectedTalkTheme);
+}
