c072cc74961de36858f2c37e05950106f5b753ea
maiamea <maiameawork@gmail.com>
2022-07-27 17:43:06 +0900

    feat: トークテーマをランダムに表示する

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import talkThemesData from "./talk-themes-data.json" assert {type: "json"}
-console.log({talkThemesData});
 
 const startButton = document.getElementById('start-button');
 
@@ -13,8 +12,33 @@ startButton.onclick = () => {
   const resultArea = document.getElementById('result-area');
   const header = document.createElement('h2');
   header.innerHTML = 'トークテーマの発表';
-  const selectedTalkTheme = document.createElement('p');
-  selectedTalkTheme.innerHTML = 'トークテーマは「昨日の晩ごはん」です。';
+  const paragraph = document.createElement('p');
+  paragraph.innerHTML =  randomTalkTheme(talkThemesData);
   resultArea.appendChild(header);
-  resultArea.appendChild(selectedTalkTheme);
+  resultArea.appendChild(paragraph);
+}
+
+// トークテーマの合計数
+const sumTalkThemes = talkThemesData.length;
+
+/**
+ * 乱数を生成
+ * @param {number} sumTalkThemes 
+ * @returns {number} ランダムな数字を返す
+ */
+function makeRandomNumber(sumTalkThemes) {
+  return Math.floor(Math.random() * sumTalkThemes);
+}
+
+
+/**
+ * ランダムに選ばれたトークテーマを返す
+ * @param {Array} talkThemesData トークテーマの配列
+ * @returns {string} ランダムに選ばれた1つのトークテーマ
+ */
+function randomTalkTheme(talkThemesData) {
+  console.log({talkThemesData});
+  const randomIndex =  makeRandomNumber(sumTalkThemes);
+  console.log({randomIndex});
+  return talkThemesData[randomIndex];
 }
