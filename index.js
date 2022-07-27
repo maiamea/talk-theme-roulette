17ecda68178a38649bc1be54cb9f2f4c138a4530
maiamea <maiameawork@gmail.com>
2022-07-27 22:17:14 +0900

    refactor: 決定したトークテーマを表示するエリアの作成処理を、makeTalkThemeArea関数に切り出す

--- index.js
+++ index.js
@@ -3,16 +3,24 @@
 import talkThemesData from "./talk-themes-data.json" assert {type: "json"}
 
 const startButton = document.getElementById('start-button');
-
 const resultArea = document.getElementById('result-area');
 
+// トークテーマの合計数
+const sumTalkThemes = talkThemesData.length;
 
 // スタートボタンをクリックすると、トークテーマが表示される
 startButton.onclick = () => {
   console.log('ボタンが押されました。');
   
-  
   // 決定したトークテーマを表示するエリアの作成
+  makeTalkThemeArea();
+}
+
+
+/**
+ * 決定したトークテーマを表示するエリアの作成
+ */
+function makeTalkThemeArea() {
   resultArea.innerText = ''; // すでに表示されてる結果を削除する
   const header = document.createElement('h2');
   const paragraph = document.createElement('p');
@@ -20,18 +28,6 @@ startButton.onclick = () => {
   paragraph.innerText =  randomTalkTheme(talkThemesData);
   resultArea.appendChild(header);
   resultArea.appendChild(paragraph);
-}
-
-// トークテーマの合計数
-const sumTalkThemes = talkThemesData.length;
-
-/**
- * 乱数を生成
- * @param {number} sumTalkThemes 
- * @returns {number} ランダムな数字を返す
- */
-function makeRandomNumber(sumTalkThemes) {
-  return Math.floor(Math.random() * sumTalkThemes);
 }
 
 
@@ -45,4 +41,14 @@ function randomTalkTheme(talkThemesData) {
   const randomIndex =  makeRandomNumber(sumTalkThemes);
   console.log({randomIndex});
   return talkThemesData[randomIndex];
+}
+
+
+/**
+ * 乱数を生成
+ * @param {number} sumTalkThemes 
+ * @returns {number} ランダムな数字を返す
+ */
+function makeRandomNumber(sumTalkThemes) {
+  return Math.floor(Math.random() * sumTalkThemes);
 }
